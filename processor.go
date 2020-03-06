e5fe61b94516b7c905f54d57b96deaed8d3b4aa7
Aditi Choksi <mail.aditichoksi@gmail.com>
2020-03-06 08:13:02 -0500

    working code with all changes

--- processor.go
+++ processor.go
@@ -7,7 +7,6 @@ package main
 // os: used to read command line arguments.
 // encoding/json: used to marshal / unmarshal json input as needed.
 
-
 import (  
 	"fmt"
 	"io/ioutil"
@@ -15,8 +14,6 @@ import (
 	"os"
 	"encoding/json"
 )
-
-var call_number = 0
 
 // A structure that defines the a Push Down Automata
 // This class contains the attributes of a PDA
@@ -49,8 +46,7 @@ func pop(p *PDAProcessor) {
 }
 
 // Function to obtain the top n elements of the stack. This function does not modify the stack.
-func peek(p PDAProcessor, k int) []string {
-	call_number = call_number + 1
+func peek(p *PDAProcessor, k int) []string {
 	top := [] string{}
 	l := len(p.Stack)
 	if (l <= k) {
@@ -64,183 +60,256 @@ func peek(p PDAProcessor, k int) []string {
 }
 
 // Function to reset the PDA and the stack. This deletes everything from the stack 
-// and sets the current state to the start state so that we can start anew.
+// and sets the current state to the start state so that we can start new.
 func reset(p *PDAProcessor) {
-	call_number = call_number + 1
 	p.Stack = make([]string, 0)
 	p.Current_State = p.Pda.Start_state
 }
 
-// Function to open the grammar file. This function opens the json file, reads it and 
-// unmarshal's its input into the PDA structure.
-func open(fn string, p *Pda) bool {
-	call_number = call_number + 1
-	dat, err := ioutil.ReadFile(fn)
-	if err != nil {
-		fmt.Print(err)
-		return false
-    }
-
-	err = json.Unmarshal(dat, &p)
+// Function to open the grammar file. This function unmarshal's the data input into the PDA structure.
+func open(data []byte, p *Pda) bool {	
+	err := json.Unmarshal(data, &p)
 	if err != nil {
 		fmt.Print(err)
 		return false
 	}
-	
 	return true
 }
 
 // Function to check if the input string has been accepted by the pda 
-func is_accepted(proc PDAProcessor) {
-	call_number = call_number + 1
-	flag := 0
+func is_accepted(proc PDAProcessor) bool{
+	flag := false
 	accepting_states := proc.Pda.Accepting_states
 	cs := proc.Current_State
 
 	if len(proc.Stack) == 0 {
 		for i:= 0; i < len(accepting_states); i++ {
 			if cs == accepting_states[i] {
-				flag = 1
-				fmt.Println("Input token Accepted")
-				done(proc, true)
+				flag = true
+				fmt.Println("\n***************************")
+				fmt.Println("Input token Accepted.")
 				break
 			}
 		}
 	}
-
-	if flag == 0 {
-		fmt.Println("Input token Rejected")
-		done(proc, false)
+	if !flag {
+		fmt.Println("\n***************************")
+		fmt.Println("Input string Rejected.")
 	}
+	return flag
 }
 
 // The done returns the final status of the current state and the stack after the input string is processed.
-func done(proc PDAProcessor, is_accepted bool){
-	call_number = call_number + 1
-	fmt.Println("call_number: ", call_number)
-	fmt.Println("pda_name: ", proc.Pda.Name)
-	fmt.Println("is_method_accepted: ", is_accepted)
-	fmt.Println("current_state: ", proc.Current_State)
-	fmt.Println("stack_symbols", peek(proc, 5))
+func done(proc PDAProcessor, is_accepted bool, transition_count int) {
+	fmt.Println("pda = ", proc.Pda.Name,"::total_clock = ", transition_count, "::method = is_accepted = ", is_accepted,"::Current State = ", current_state(proc))
+	fmt.Println("Current_state: ", current_state(proc))
+	fmt.Println(proc.Stack)
 }
 
+// Returns the current state of the PDA
+func current_state(proc PDAProcessor) string{
+	return proc.Current_State
+}
 
 // This function accepts the input string and performs the necessary transitions and 
 // stack operations for every token,
-func put(proc PDAProcessor, p Pda, s string) int {
-	call_number = call_number + 1
-
-	inp_len := len(s)
+func put(proc *PDAProcessor, s string) int {
+	
+	var p Pda = proc.Pda
 	transitions := p.Transitions
 	tran_len := len(transitions)
 	transition_count := 0
-
-	proc.Current_State = p.Start_state
-	currentStackSymbol := "null"
-	i := 0
-	for ; i < inp_len; i++ {
+	for j := 0; j < tran_len; j++ {
+		var allowed_current_state = transitions[j][0]
+		var input = transitions[j][1]
+		var allowed_top_of_stack = transitions[j][2]
+		var target_state = transitions[j][3]
+		var action_item = transitions[j][4]
+		var currentStackSymbol = ""
+		var top = peek(proc, 1)
+		if(len(top)>=1){
+			currentStackSymbol = top[0]
+		}
 		
-		char := string(s[i])
-		matching_transition := false
-		for j := 0; j < tran_len; j++ {
-			t := transitions[j]
-			transition_count = check_for_dead_moves(t, &proc, transition_count) 
-			if t[0] == proc.Current_State && t[1] == char && t[2] == currentStackSymbol {
-				matching_transition = true
-				proc.Current_State = t[3]
+		// PDA is deterministic. It jumps from current state to target state in the specified conditions
+		if (input == "null" && allowed_current_state == proc.Current_State && allowed_top_of_stack == "null" && action_item == "null") {
+			fmt.Println("Current State ",proc.Current_State)
+			fmt.Println("No push/pop performed...... Processed dead transition")
+			fmt.Println("Stack: ", proc.Stack)
+			fmt.Println("New State ", target_state)
+			proc.Current_State = target_state
+			transition_count = transition_count + 1
+		}
 
-				if t[4] != "null" {
-					push(&proc, t[4])
-				} else {
-					pop(&proc)
-				}
+		if (allowed_current_state == proc.Current_State && input == s)  {
 
-				top := peek(proc, 1)[0]
-				currentStackSymbol = top
+			//Perform Push action
+			if action_item != "null" && allowed_top_of_stack == "null" {
+				fmt.Println("Current State ", proc.Current_State)
+				fmt.Println("Push ", action_item, " on the stack.")
+				fmt.Println("New State ", target_state)
+				fmt.Println("Stack: ", proc.Stack)
+				transition_count = transition_count + 1
+				proc.Current_State = target_state
+				push(proc, action_item)
+
+				break
+				//performs Push action
+			} else if action_item != "null" &&  allowed_top_of_stack == currentStackSymbol {
+				fmt.Println("Current State ",proc.Current_State)
+				fmt.Println("Push ", action_item, " on the stack")
+				fmt.Println("New State ", target_state)
+				fmt.Println("Stack: ", proc.Stack)
+				transition_count = transition_count + 1
+				proc.Current_State = target_state
+				push(proc, action_item)
+				break
+				//performs Pop action
+			} else if action_item == "null" &&  allowed_top_of_stack == currentStackSymbol {
+				pop(proc)
+				fmt.Println("Current State ",proc.Current_State)
+				fmt.Println("Pop top of the stack.")
+				fmt.Println("New State ",target_state)
+				fmt.Println("Stack: ", proc.Stack)
+				transition_count = transition_count + 1
+				proc.Current_State = target_state
+				break
+				//Neither push nor pop action required
+			} else if allowed_top_of_stack == "null" {
+				fmt.Println("Current State ",proc.Current_State)
+				fmt.Println("No push/pop performed...... Consumed input token")
+				fmt.Println("New State ",target_state)
+				fmt.Println("Stack: ", proc.Stack)
+				proc.Current_State = target_state
 				transition_count = transition_count + 1
 				break
-			}	       
-		}
-
-		if (!matching_transition) {
-			break
-		}
-	}
-	if i == inp_len {
-		transition_count = eos(proc,transition_count)
-	} else {
-		is_accepted(proc)
+			}
+		}	       
 	}
 
+	fmt.Println("Clock count for consuming the input token = ", transition_count)
 	return transition_count
 }
 
 // Performs the last transition to move the Automata to accepting state after the input
 // string has been successfully parsed. 
-func eos(proc PDAProcessor, transition_count int)int {
-	call_number = call_number + 1
+func eos(proc *PDAProcessor) {
 	length_of_stack := len(proc.Stack)
 	allowed_transitions := proc.Pda.Transitions
 	target_state := ""
-
-	for j := 0; j < len(allowed_transitions); j++ {
+	allowed_top_of_stack := ""
+	var currentStackSymbol = ""
+	var top = peek(proc, 1)
+	if(len(top)>=1){
+		currentStackSymbol = top[0]
+	}
+	for j := 0; j < len(allowed_transitions); j++ {	
 		var allowed_current_state = allowed_transitions[j][0]
-		if allowed_current_state == proc.Current_State {
+		allowed_top_of_stack = allowed_transitions[j][2]
+		
+		if allowed_current_state == proc.Current_State && allowed_top_of_stack == currentStackSymbol{
 			target_state = allowed_transitions[j][3]
+			break
 		}
 	}
-
-	if peek(proc, 1)[0] == proc.Pda.Eos {
+	if currentStackSymbol == proc.Pda.Eos {
+		fmt.Println("")
+		fmt.Println("Popping last $ from the stack")
+		fmt.Println("Current State ",proc.Current_State)
+		fmt.Println("New State ",target_state)
 		proc.Current_State = target_state
 		if length_of_stack > 0 {
-			pop(&proc)
+			pop(proc)
 		}
-		transition_count = transition_count + 1
 	}
-	is_accepted(proc)
-	return transition_count
 }
 
 // Pushes initial EOS token into the stack and moves to the next state indicating
 // the start of transitions
-func check_for_dead_moves(transition []string, proc *PDAProcessor, transition_count int) int{
-	allowed_current_state := transition[0]
-	input := transition[1]
-	allowed_top_of_stack := transition[2]
-	target_state := transition[3]
-	action_item := transition[4]
-	if allowed_current_state == proc.Current_State && input == "null" && allowed_top_of_stack == "null"{
-        proc.Current_State = target_state
-        push(proc, action_item)
-        transition_count = transition_count + 1
+func check_for_first_move(proc *PDAProcessor, transition_count int) int{
+	allowed_transitions := proc.Pda.Transitions
+	target_state := ""
+	input := ""
+	allowed_top_of_stack := ""
+	action_item := ""
+	
+	for j := 0; j < len(allowed_transitions); j++ {
+		var allowed_current_state = allowed_transitions[j][0]
+		if allowed_current_state == proc.Current_State {
+			input = allowed_transitions[j][1]
+			allowed_top_of_stack = allowed_transitions[j][2]
+			target_state = allowed_transitions[j][3]
+			action_item = allowed_transitions[j][4]
+			break
+		}
+	}
+	
+	if input == "null" && allowed_top_of_stack == "null"{
+		fmt.Println("Current State ", proc.Current_State)
+
+		push(proc, action_item)
+		fmt.Println("Pushing $ on the stack")
+
+		proc.Current_State = target_state
+		fmt.Println("New State ", proc.Current_State)
+        
+		transition_count = transition_count + 1
+		fmt.Println()
 	} 
 	return transition_count
+}
+
+//Checks whether the input string is composed of the allowed characters. 
+func verify_Input_String(proc PDAProcessor, input_string string)bool{
+	var input_symbols = proc.Pda.Input_alphabet
+	verify:=false
+	for i :=0; i < len(input_string); i++ {
+		verify=false
+		for j :=0; j < len(input_symbols); j++ {
+			if string(input_string[i]) == input_symbols[j] {
+				verify = true
+				break
+			}
+		}
+		
+		if verify == false {
+			break
+		}
+	}
+	return verify
 }
 
 
 // main function to start input processing. It reads the grammar json file
 // to create an instance of PDA. The input is obtained either from a text file or
 // standard input.
+
 func main(){
 
 	fn := os.Args[1]
 
 	var p Pda
-	open(fn, &p)
+	data, err := ioutil.ReadFile(fn)
+	if err != nil {
+		fmt.Println(err)
+		return;
+    }
+	open(data, &p)
 
 	proc := PDAProcessor {
 		Pda: p,
 	}
 	reset(&proc)
+
 	transition_count := 0
-	input_token := ""
+	input_string := ""
 
 	if len(os.Args) < 3 {
 
 		fmt.Print("Enter input string: ")
 		scanner := bufio.NewScanner(os.Stdin)
 		scanner.Scan()
-		input_token = scanner.Text()
+		input_string = scanner.Text()
 	} else {
 		inp := os.Args[2]
 		file, err := os.Open(inp)
@@ -249,15 +318,39 @@ func main(){
 		n, err := file.Read(input)
 		if err != nil {
 			fmt.Print(err)
+			return;
 		}
-		input_token = string(input[:n])
+		input_string = string(input[:n])
 	}
+	fmt.Print("\n----------------------------------")
+	fmt.Println("Input string: ", input_string)
+	if verify_Input_String(proc,input_string) {
+		if input_string != "" {
+			transition_count = check_for_first_move(&proc, transition_count)
+			inp_len := len(input_string)
+			i := 0
+			for ; i < inp_len; i++ {
+				count:=0
+				char := string(input_string[i])
+				fmt.Println("\n***************************")
+				fmt.Println("Input Token: ", char)
+				count = put(&proc, char)
+				if  count == 0 {
+					fmt.Println("Input token not processed.", char)
+					break
+				} else {
+					transition_count = transition_count + count
+				}
+			}
+			//End of input string reached
+			if i == inp_len {
+				eos(&proc)			
+			}
 
-	if input_token == "" {
-		is_accepted(proc)
-	} else{
-		transition_count = put(proc, p, input_token)
+			isAccepted := is_accepted(proc)
+			done(proc, isAccepted, transition_count)
+		}
+	} else {
+		fmt.Print("Invalid Input String. It should be composed of only the symbols in ",proc.Pda.Input_alphabet)
 	}
-
-	fmt.Println("Number of transitions = ", transition_count)
 }
